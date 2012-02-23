a1135fe24829157891caf69183c097b25836e937
Tucker Trainor <tucker@wingedmouse.com>
2012-02-23 15:01:24 -0500

    added ThreadCounter class to help manage number of Robot threads

---
+++ SimpleSim/ThreadCounter.java
@@ -0,0 +1,11 @@
+/**
+ * File: ThreadCounter.java
+ * @author: Tucker Trainor <tmt33@pitt.edu>
+ *
+ * Class to handle "global" counting of RobotThread instances
+ */
+
+public class ThreadCounter {
+	// the number of RobotThreads active
+	public static int robotThreads = 0;
+}
