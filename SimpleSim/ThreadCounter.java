848e177316221e293ef510a2941622b0f67405ce
Tucker Trainor <tucker@wingedmouse.com>
2012-02-27 11:17:04 -0500

    updated ThreadCounter

--- SimpleSim/ThreadCounter.java
+++ SimpleSim/ThreadCounter.java
@@ -6,6 +6,10 @@
  */
 
 public class ThreadCounter {
-	// the number of RobotThreads active
-	public static int robotThreads = 0;
+	// the number of RobotThreads that have been run so far
+	public static int threadCount = 0;
+	// the number of active RobotThreads
+	public static int activeThreads = 0;
+	// the number of threads in total to be run (from parameters file)
+	public static int maxThreads = 0;
 }
