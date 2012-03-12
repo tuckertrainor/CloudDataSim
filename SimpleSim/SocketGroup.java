1e2e48e1baff7bffd860b6333ed1cb694bf0adb6
Tucker Trainor <tucker@wingedmouse.com>
2012-03-12 13:30:13 -0400

    building class

---
+++ SimpleSim/SocketGroup.java
@@ -0,0 +1,21 @@
+/**
+ * File: SocketGroup.java
+ * @author: Tucker Trainor <tmt33@pitt.edu>
+ *
+ * A class to hold active socket connections
+ */
+
+import java.util.*;
+import java.net.Socket;
+
+public class SocketGroup {
+	private Hashtable<Integer, Socket> list = new Hashtable<Integer, Socket>();
+	
+	public void addSocket(int serverNum, Socket socket) {
+		list.put(serverNum, socket);
+	}
+	
+	public Socket getSocket(int serverNum) {
+		return list.get(serverNum);
+	}
+}
