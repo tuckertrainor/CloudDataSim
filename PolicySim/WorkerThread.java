aaeed1b762af8fb649e3719ba77e98c6d9f7ea2b
Tucker Trainor <tucker@wingedmouse.com>
2012-03-21 12:28:46 -0400

    deferred looks complete

--- PolicySim/WorkerThread.java
+++ PolicySim/WorkerThread.java
@@ -84,7 +84,8 @@ public class WorkerThread extends Thread {
 					else if (query[0].equals("R")) { // READ
 						// Check server number, perform query or pass on
 						if (Integer.parseInt(query[2]) == serverNumber) { // Perform query on this server
-							// Check that if a fresh Policy version is needed, it is gotten
+							// Check that if a fresh Policy version is needed
+							// (e.g. if this query has been passed in) it is set
 							if (transactionPolicyVersion == 0) {
 								transactionPolicyVersion = refreshPolicy();
 							}
@@ -150,7 +151,6 @@ public class WorkerThread extends Thread {
 					}
 					else if (query[0].equals("POLICY")) { // POLICY
 						// Return Policy version on this server to caller
-						System.out.println("*** POLICY CALL " + transactionPolicyVersion);
 						msgText = "VERSION " + Integer.toString(transactionPolicyVersion);
 					}
 					else if (query[0].equals("C")) { // COMMIT
@@ -353,10 +353,8 @@ public class WorkerThread extends Thread {
 						resp = (Message)sockList.get(serverNum).input.readObject();
 						// Compare Policy versions
 						String msgSplit[] = resp.theMessage.split(" ");
-						System.out.println("*** " + msgSplit[0] + " " + msgSplit[1]);
 						if (msgSplit[0].equals("VERSION") && Integer.parseInt(msgSplit[1]) < masterPolicyVersion) {
 							stale++;
-							System.out.println("*** STALE INCREASED on " + serverNum);
 						}
 					}
 					catch (Exception e) {
