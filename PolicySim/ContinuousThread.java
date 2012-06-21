70dd203b9b91fdc8bb26f4b759a0fac324590f46
Tucker Trainor <tucker@wingedmouse.com>
2012-06-21 09:37:36 -0400

    changed PASS/FAIL to TRUE/FALSE where referring to proof success/failure

--- PolicySim/ContinuousThread.java
+++ PolicySim/ContinuousThread.java
@@ -30,21 +30,21 @@ public class ContinuousThread extends IncrementalThread {
 	
 	/* View consistency
 	 * 1.  get freshest policy off own server
-	 * 2.  perform first operation, double check local policy, perform proof, perform operation, PASS,ACK or FAIL
+	 * 2.  perform first operation, double check local policy, perform proof, perform operation, TRUE,ACK or FALSE
 	       how do we double check local policy on own server without involving PS?
 	 * 3.  repeat if next operation on coordinator's server, else have participant join
 	 
 	 * 4.  have server join (and set up update condition)
 	 * 5.  coordinator sends its txn policy version and operation to server
-	 * 6.  server compares its policy to coordinator's, uses freshest for proof then runs operation, returns PASS, ACK, v. or FAIL, v.
+	 * 6.  server compares its policy to coordinator's, uses freshest for proof then runs operation, returns TRUE, ACK, v. or FALSE, v.
 	 
-	 * 7.  coordinator receives PASS, ACK, v. or FAIL, v., compares v. to own version
+	 * 7.  coordinator receives TRUE, ACK, v. or FALSE, v., compares v. to own version
 	 * 8.  if v. is fresher, update txn policy version, run 2PV starting on own server
 	 
 	 * 9.  2PV: send txn policy version to all participants, along with 2PV msg
 	 *	   just received freshest from server S, send it back to him anyway? eat cost of 2-way msg?
 	 * 10. each participant checks txn policy version, if < v. then sets it to v. and re-runs auths
-	 * 11. sends back PASS or FAIL along with v.
+	 * 11. sends back TRUE or FALSE along with v.
 	 *     should we have a different response if policy was up to date?
 	 * 12. coordinator gathers policy versions again, repeats 2PV if fresher is found
 	 
@@ -148,8 +148,8 @@ public class ContinuousThread extends IncrementalThread {
 							
 							// Check transaction policy against server policy
 							if (checkLocalAuth() == false) {
-								msgText = "ABORT LOCAL_POLICY_FAIL";
-								System.out.println("ABORT LOCAL_POLICY_FAIL: " +
+								msgText = "ABORT LOCAL_POLICY_FALSE";
+								System.out.println("ABORT LOCAL_POLICY_FALSE: " +
 												   "READ for txn " + query[1] +
 												   " sequence " + query[3]);
 							}
@@ -183,14 +183,14 @@ public class ContinuousThread extends IncrementalThread {
 							if (msgText.equals("ACK")) { // join() did not fail above
 								/* View consistency:
 								 * Send transactionPolicyVersion, operation
-								 * Rec'v PASS, ACK, policy used or FAIL, policy used
+								 * Rec'v TRUE, ACK, [policy] or FALSE, [policy]
 								 */
 
 								// The other server has joined - send passQuery message, parse response
 								String response = passQuery(Integer.parseInt(query[2]), queryGroup[i]);
-								// Expecting PASS ACK POLICY or FAIL POLICY
+								// Expecting TRUE ACK POLICY or FALSE POLICY
 								String respSplit[] = response.split(" ");
-								if (respSplit[0].equals("PASS") && respSplit[1].equals("ACK")) {
+								if (respSplit[0].equals("TRUE") && respSplit[1].equals("ACK")) {
 									// Check policy returned from server
 									int returnPolicy = Integer.parseInt(respSplit[2]);
 									if (returnPolicy > transactionPolicyVersion) {
@@ -200,18 +200,18 @@ public class ContinuousThread extends IncrementalThread {
 															   " sequence " + query[3] +
 															   " policy version " + returnPolicy);
 										}
-										else { // FAIL returned
-											msgText = "ABORT LOCAL_POLICY_FAIL_2PV";
-											System.out.println("ABORT LOCAL_POLICY_FAIL_2PV: " +
+										else { // FALSE returned
+											msgText = "ABORT LOCAL_POLICY_FALSE_2PV";
+											System.out.println("ABORT LOCAL_POLICY_FALSE_2PV: " +
 															   " txn " + query[1] +
 															   " sequence " + query[3] +
 															   ", policy version " + returnPolicy);
 										}
 									}
 								}
-								else { // FAIL
-									msgText = "ABORT LOCAL_POLICY_FAIL";
-									System.out.println("ABORT LOCAL_POLICY_FAIL: " +
+								else { // FALSE
+									msgText = "ABORT LOCAL_POLICY_FALSE";
+									System.out.println("ABORT LOCAL_POLICY_FALSE: " +
 													   "READ for txn " + query[1] +
 													   " sequence " + query[3] +
 													   ", policy version " + respSplit[1]);
@@ -240,8 +240,8 @@ public class ContinuousThread extends IncrementalThread {
 							
 							// Check transaction policy against server policy
 							if (checkLocalAuth() == false) {
-								msgText = "ABORT LOCAL_POLICY_FAIL";
-								System.out.println("ABORT LOCAL_POLICY_FAIL: " +
+								msgText = "ABORT LOCAL_POLICY_FALSE";
+								System.out.println("ABORT LOCAL_POLICY_FALSE: " +
 												   "WRITE for txn " + query[1] +
 												   " sequence " + query[3]);
 							}
@@ -275,14 +275,14 @@ public class ContinuousThread extends IncrementalThread {
 							if (msgText.equals("ACK")) { // join() did not fail above
 								/* View consistency:
 								 * Send transactionPolicyVersion, operation
-								 * Rec'v PASS, ACK, policy used or FAIL, policy used
+								 * Rec'v TRUE, ACK, policy used or FALSE, policy used
 								 */
 								
 								// The other server has joined - send passQuery message, parse response
 								String response = passQuery(Integer.parseInt(query[2]), queryGroup[i]);
-								// Expecting PASS ACK POLICY or FAIL POLICY
+								// Expecting TRUE ACK [policy] or FALSE [policy]
 								String respSplit[] = response.split(" ");
-								if (respSplit[0].equals("PASS") && respSplit[1].equals("ACK")) {
+								if (respSplit[0].equals("TRUE") && respSplit[1].equals("ACK")) {
 									// Check policy returned from server
 									int returnPolicy = Integer.parseInt(respSplit[2]);
 									if (returnPolicy > transactionPolicyVersion) {
@@ -293,17 +293,17 @@ public class ContinuousThread extends IncrementalThread {
 															   " policy version " + returnPolicy);
 										}
 										else {
-											msgText = "ABORT LOCAL_POLICY_FAIL_2PV";
-											System.out.println("ABORT LOCAL_POLICY_FAIL_2PV: " +
+											msgText = "ABORT LOCAL_POLICY_FALSE_2PV";
+											System.out.println("ABORT LOCAL_POLICY_FALSE_2PV: " +
 															   " txn " + query[1] +
 															   " sequence " + query[3] +
 															   ", policy version " + returnPolicy);
 										}
 									}
 								}
-								else { // FAIL
-									msgText = "ABORT LOCAL_POLICY_FAIL";
-									System.out.println("ABORT LOCAL_POLICY_FAIL: " +
+								else { // FALSE
+									msgText = "ABORT LOCAL_POLICY_FALSE";
+									System.out.println("ABORT LOCAL_POLICY_FALSE: " +
 													   "WRITE for txn " + query[1] +
 													   " sequence " + query[3] +
 													   ", policy version " + respSplit[1]);
@@ -330,8 +330,8 @@ public class ContinuousThread extends IncrementalThread {
 						
 						// Run proof of authorization
 						if (checkLocalAuth() == false) {
-							msgText = "FAIL LOCAL_POLICY_FAIL";
-							System.out.println("ABORT LOCAL_POLICY_FAIL: " +
+							msgText = "FALSE LOCAL_POLICY_FALSE";
+							System.out.println("ABORT LOCAL_POLICY_FALSE: " +
 											   "READ for txn " + query[1] +
 											   " sequence " + query[3]);
 						}
@@ -339,8 +339,8 @@ public class ContinuousThread extends IncrementalThread {
 							System.out.println("READ for txn " + query[1] +
 											   " sequence " + query[3]);
 							databaseRead();
-							// Respond with PASS ACK [policy version]
-							msgText = "PASS ACK " + transactionPolicyVersion;
+							// Respond with TRUE ACK [policy]
+							msgText = "TRUE ACK " + transactionPolicyVersion;
 							// Add to query log
 							if (addToQueryLog(query, transactionPolicyVersion)) {
 								System.out.println("Transaction " + query[1] +
@@ -371,8 +371,8 @@ public class ContinuousThread extends IncrementalThread {
 						
 						// Run proof of authorization
 						if (checkLocalAuth() == false) {
-							msgText = "FAIL LOCAL_POLICY_FAIL";
-							System.out.println("ABORT LOCAL_POLICY_FAIL: " +
+							msgText = "FALSE LOCAL_POLICY_FALSE";
+							System.out.println("ABORT LOCAL_POLICY_FALSE: " +
 											   "READ for txn " + query[1] +
 											   " sequence " + query[3]);
 						}
@@ -380,8 +380,8 @@ public class ContinuousThread extends IncrementalThread {
 							System.out.println("WRITE for txn " + query[1] +
 											   " sequence " + query[3]);
 							databaseWrite();
-							// Respond with PASS ACK [policy version]
-							msgText = "PASS ACK " + transactionPolicyVersion;
+							// Respond with TRUE ACK [policy]
+							msgText = "TRUE ACK " + transactionPolicyVersion;
 							// Add to query log
 							if (addToQueryLog(query, transactionPolicyVersion)) {
 								System.out.println("Transaction " + query[1] +
@@ -418,7 +418,7 @@ public class ContinuousThread extends IncrementalThread {
 						// YES TRUE [greater of policy versions]
 						if (integrityCheck()) { // If integrity check passes
 							String result2PV = answer2PV(Integer.parseInt(query[1]));
-							if (result2PV.indexOf("PASS") != -1) { // 2PV successful
+							if (result2PV.indexOf("TRUE") != -1) { // 2PV successful
 								msgText = "YES TRUE " + Integer.parseInt(query[1]);
 							}
 							else {
@@ -433,8 +433,8 @@ public class ContinuousThread extends IncrementalThread {
 						// receives: 2PV [policy from coord]
 						// Reruns auths (if necessary) with greater of local/coord policy
 						// returns:
-						// PASS [policy version used]
-						// FAIL [policy version used]
+						// TRUE [policy]
+						// FALSE [policy]
 						msgText = answer2PV(Integer.parseInt(query[1]));
 					}
 					else if (query[0].equals("RSERV")) { // Random server for policy pushing
@@ -491,7 +491,7 @@ public class ContinuousThread extends IncrementalThread {
 	public String passQuery(int otherServer, String query) {
 		/* View consistency:
 		 * Send transactionPolicyVersion, operation
-		 * Rec'v PASS, ACK, policy used or FAIL, policy used
+		 * Rec'v TRUE, ACK, policy used or FALSE, policy used
 		 */
 		Message msg = null;
 		
@@ -519,7 +519,7 @@ public class ContinuousThread extends IncrementalThread {
 			System.err.println("Error during passQuery(): " + e.getMessage());
 			e.printStackTrace(System.err);
 		}
-		return "FAIL";
+		return "PASSQUERY_FAIL";
 	}
 	
 	/**
@@ -615,7 +615,7 @@ public class ContinuousThread extends IncrementalThread {
 			transactionPolicyVersion = my_tm.getPolicy();
 			
 			if (!run2PVC(transactionPolicyVersion)) {
-				return "ABORT 2PVC_FAIL";
+				return "ABORT 2PVC_FALSE";
 			}
 		}
 		else {
@@ -688,7 +688,7 @@ public class ContinuousThread extends IncrementalThread {
 									   ", seq " + queryLog.get(j).getSequence() +
 									   " with policy v. " + transactionPolicyVersion +
 									   " (was v. " + queryLog.get(j).getPolicy() +
-									   "): FAIL");
+									   "): FALSE");
 					return false; // (authorization failed)
 				}
 				else {
@@ -697,7 +697,7 @@ public class ContinuousThread extends IncrementalThread {
 									   ", seq " + queryLog.get(j).getSequence() +
 									   " with policy v. " + transactionPolicyVersion +
 									   " (was v. " + queryLog.get(j).getPolicy() +
-									   "): PASS");
+									   "): TRUE");
 					// Update policy version used for proof
 					queryLog.get(j).setPolicy(transactionPolicyVersion);
 				}
@@ -721,10 +721,10 @@ public class ContinuousThread extends IncrementalThread {
 						System.out.println("Response of server " + serverNum +
 										   " for message 2PV " + policyVersion +
 										   ": " + msg.theMessage);
-						// Parse response: PASS [policy version] or FAIL [policy version]
+						// Parse response: TRUE [policy] or FALSE [policy]
 						String msgSplit[] = msg.theMessage.split(" ");
 						recdPolicy = Integer.parseInt(msgSplit[1]);
-						if (msgSplit[0].equals("PASS")) {
+						if (msgSplit[0].equals("TRUE")) {
 							// Check for fresher policies than policyVersion
 							if (recdPolicy > transactionPolicyVersion) {
 								// Set transaction policy, start over
@@ -732,7 +732,7 @@ public class ContinuousThread extends IncrementalThread {
 							}
 						}
 						else {
-							return false; // Proof FAIL
+							return false; // Proof FALSE
 						}
 					}
 					catch (Exception e) {
@@ -752,7 +752,7 @@ public class ContinuousThread extends IncrementalThread {
 	 *
 	 * @param int - the policy version to perform proofs with
 	 *
-	 * @return String - the result of the process (PASS/FAIL [policy version]
+	 * @return String - the result of the process (TRUE/FALSE [policy version]
 	 */
 	public String answer2PV(int coordPolicy) {
 		if (coordPolicy > transactionPolicyVersion) { // Re-run proofs with fresher policy
@@ -768,8 +768,8 @@ public class ContinuousThread extends IncrementalThread {
 									   ", seq " + queryLog.get(j).getSequence() +
 									   " with policy v. " + transactionPolicyVersion +
 									   " (was v. " + queryLog.get(j).getPolicy() +
-									   "): FAIL");
-					return "YES FALSE " + transactionPolicyVersion; // (authorization failed)
+									   "): FALSE");
+					return "FALSE " + transactionPolicyVersion; // (authorization failed)
 				}
 				else {
 					System.out.println("Authorization of " + queryLog.get(j).getQueryType() +
@@ -777,13 +777,13 @@ public class ContinuousThread extends IncrementalThread {
 									   ", seq " + queryLog.get(j).getSequence() +
 									   " with policy v. " + transactionPolicyVersion +
 									   " (was v. " + queryLog.get(j).getPolicy() +
-									   "): PASS");
+									   "): TRUE");
 					// Update policy version used for proof
 					queryLog.get(j).setPolicy(transactionPolicyVersion);
 				}
 			}
 		}
-		return "PASS " + transactionPolicyVersion;
+		return "TRUE " + transactionPolicyVersion;
 	}
 	
 	/**
